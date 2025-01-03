ed6e0493fd2f2f4e11b60de3611294ff9630846d
SuzyUwU <yudhapratamaputra85@gmail.com>
2025-01-03 18:02:42 +0700

    Credit update

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import View.Menus.MainMenuGUI;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("topic: Computer Simulation \n\n credits Z:\n Gitarivo Frizqi Putranda Cavalera \n Yudha Pratama Putra \n Lubna Sajidah \n Nazwa Dewanti Asyifa \n Muhammad Husaini Rahmatulloh \n code made by love from :\n Group 5 PBO");
+        System.out.println("topic: Computer Simulation \n\n credits:\n Gitarivo Frizqi Putranda Cavalera (23051204106) \n Yudha Pratama Putra (23051204108) \n Lubna Sajidah (23051204111) \n Nazwa Dewanti Asyifa (23051204119) \n Muhammad Husaini Rahmatulloh (23051204125) \n\n code made by love from :\n Group 5 PBO");
 
         UserProfile userProfile = DataHandler.loadData();
         LoginMenuGUI loginMenu = new LoginMenuGUI(userProfile);
