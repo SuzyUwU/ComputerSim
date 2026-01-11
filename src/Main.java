4be6c3fbba408bc82088b6fab0d5e9f6f0056bfd
SuzyUwU <yudhapratamaputra85@gmail.com>
2026-01-11 18:50:11 +0700

    delete unused line

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,6 @@ import View.Menus.MainMenuGUI;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("topic: Computer Simulation \n code made by : Yudha Pratama Putra \n\n credits:\n Gitarivo Frizqi Putranda Cavalera (23051204106) \n Yudha Pratama Putra (23051204108) \n Lubna Sajidah (23051204111) \n Nazwa Dewanti Asyifa (23051204119) \n Muhammad Husaini Rahmatulloh (23051204125) \n\n code made by love from :\n Group 5 PBO");
-
         UserProfile userProfile = DataHandler.loadData();
         LoginMenuGUI loginMenu = new LoginMenuGUI(userProfile);
         loginMenu.out();
